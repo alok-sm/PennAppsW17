01b4754c75c9b7cf15e5a4f41b04b3f6c158640d
Alok Mysore <alok.shankar.m@gmail.com>
2017-01-21 10:32:12 -0500

    add trigger support on server

--- alexaweb/app.py
+++ alexaweb/app.py
@@ -12,6 +12,7 @@ import redis
 import uuid
 import random
 import string
+import ast
 from pydub import AudioSegment
 
 	
@@ -57,7 +58,7 @@ class TriggerHandler(BaseHandler):
 	def get(self):
 		red = redis.from_url(redis_url)
 		print red.get(self.get_current_user() + "-trigger")
-		self.write({"trigger": bool(red.get(self.get_current_user() + "-trigger"))})
+		self.write({"trigger": ast.literal_eval(red.get(self.get_current_user() + "-trigger"))})
 		red.set(self.get_current_user() + "-trigger", False)
 		self.finish()
 
