e6ca92edda12e2ccf42f350030c9eaa720bbfa79
Ubuntu <alok@VeryShadyProxy.veryshadyproxy.g3.internal.cloudapp.net>
2017-01-21 11:09:48 +0000

    remove unnedded files

--- creds.py
+++
@@ -1,9 +0,0 @@
-#Alexa
-Security_Profile_Description ="AlexaWeb"
-Security_Profile_ID  = "amzn1.application.5e1e5e9dc99c450bb56aac577df06fa9"
-Client_ID = "amzn1.application-oa2-client.0d9764a2c51d4eeaa69f732b77538bd9"
-Client_Secret = "c8b65c9c9e35030f173550ef08b79e0dae8ecca0ca9241dcc61f2d5c2b6cb29a"
-Product_ID = "AlexaWeb"
-
-#Redis
-redis_url = "127.0.0.1:6379"
